0de2b0c9cac4dc162f687727ad9804859b641e32
Ruben Bauwens <bauwens_ruben@hotmail.com>
2011-10-28 11:14:58 +0200

    Modules toegevoegd en params meegeven aan Controller

--- app/controllers/project_importer_controller.rb
+++ app/controllers/project_importer_controller.rb
@@ -14,30 +14,43 @@ def match
      @parsed_file=CSV::Reader.parse(params[:dump][:file])
      n=0
 
-sample_count = 5
-i = 0
-@samples = []
 
 
+  issue_tracking = params[:issue_tracking]
+  time_tracking = params[:time_tracking]
+  news = params[:news]
+  documents = params[:documents]
+  files = params[:files]
+  wiki = params[:wiki]
+  repository = params[:repository]
+  forums = params[:forums]
+  calendar = params[:calendar]
+  gantt = params[:gantt]
+
+  sample_count = 5
+  i = 0
+  @samples = []
      @parsed_file.each  do |row|
 				
-		if i != 0	
-   		@samples[i] = row
-		end
+		    if i != 0	
+   	    	@samples[i] = row
+		    end
 
-		if i == 0
-			@headers = row
-		end
- 
+		    if i == 0
+		    	@headers = row
+		    end
+  
 		
-		if i >= sample_count 
-			break
-		end
-i = i+1
+		    if i >= sample_count 
+		    	break
+	   	  end
+       
+        i = i+1
 		
         n=n+1       
      end
-     flash.now[:message]="CSV Import Successful,  #{n} new records added to data base"
+   end
+     #flash.now[:message]="CSV Import Successful,  #{n} new records added to data base"
    end
 
 
