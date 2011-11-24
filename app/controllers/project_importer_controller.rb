6700cfe8b51c913c0a764bb1411308a971209ab7
Ruben Bauwens <bauwens_ruben@hotmail.com>
2011-11-24 11:20:27 +0100

    Projecten worden ook aangemaakt met daarin al de correcte members

--- app/controllers/project_importer_controller.rb
+++ app/controllers/project_importer_controller.rb
@@ -49,11 +49,10 @@ class ProjectImporterController < ApplicationController
   bug_tracker = params[:bug_tracker]
   feature_tracker = params[:feature_tracker]
   support_tracker = params[:support_tracker]
-  imported_users_role = params[:role]
+  
   repository_base_url = params[:repository_url]
   sample_count = 5
-  @repository.root_url=(repository_base_url)
-  @base_url = @repository.url
+  session[:users_role] = params[:role_user]
   
   i = 0
   @samples = []
@@ -84,47 +83,77 @@ class ProjectImporterController < ApplicationController
    
   
       def result
-     @tmpfilename = session[:filename]
-      
+     tmpfilename = session[:filename]
+      role_users = Role.find_by_name(session[:users_role])
+      roles = []
+      roles << role_users
       attrs_map = params[:fields_map].invert
       
       i = 0
-      
-     
+      username_header = attrs_map['Username']
+      lastname_header = attrs_map['Lastname']
+      firstname_header = attrs_map['Firstname']
+      password_header = attrs_map['Password']
+      mail_header = attrs_map['Email']
+      groupname_header = attrs_map['Groupname']
+      counter_new_users = 0
+      counter_new_projects = 0
+    
      @headers = []
       @samplestest = []   
-     CSV.open(@tmpfilename, 'r', ',') do |row|
+      FasterCSV.foreach(tmpfilename) do |row|
+   
        if i == 0
        @headers = row
-       else
-         usernameheader = attrs_map['Username']
-         lastnameheader = attrs_map['Lastname']
-         firstnameheader = attrs_map['Firstname']
-         passwordheader = attrs_map['Password']
-         mailheader = attrs_map['Email']
-         @groupnameheader = attrs_map['Groupname']
+       @index_username = @headers.index(username_header)
+       @index_password = @headers.index(password_header)
+       @index_lastname = @headers.index(lastname_header)
+       @index_firsname = @headers.index(firstname_header)
+       @index_mail = @headers.index(mail_header)
+       @index_groupname = @headers.index(groupname_header)
+       
+       else       
+        project = Project.find_by_name(row[@index_groupname])
+        unless project
+          project = Project.new
+          project.name = row[@index_groupname]
+          project.identifier = row[@index_groupname] 
+          project.save 
+          counter_new_projects = counter_new_projects + 1       
+        end # unless project
         
-         user = User.find_by_login(row[@headers.index(usernameheader)])
+         user = User.find_by_login(row[@index_username])
          unless user
           user = User.new(:language => Setting.default_language, :mail_notification => Setting.default_notification_option)
-           user.login = row[@headers.index(usernameheader)]
-           user.password = row[@headers.index(passwordheader)]
-           user.lastname = row[@headers.index(lastnameheader)]
-           user.firstname = row[@headers.index(firstnameheader)]
-           user.password_confirmation = row[@headers.index(passwordheader)]
-           user.mail = row[@headers.index(mailheader)]
-           user.admin = 1
-            if user.save
-            user.pref.save
-         end #unless
-       @errors = user.errors.full_messages
-          end
+           user.login = row[@index_username]
+           user.password = row[@index_password]
+           user.lastname = row[@index_lastname]
+           user.firstname = row[@index_firsname]
+           user.password_confirmation = row[@index_password]
+           user.mail = row[@index_mail]
+           user.admin = 0
+           
+           
+           # r = Role.givable.find_by_id(Setting.new_project_user_role_id.to_i) || Role.givable.first
+           # m = Member.new(:user => User.current, :roles => [r])
+           # @project.members << m
+           # project.members << user
+           # user.memberships << @role_users
+           user.save
+            
+        counter_new_users = counter_new_users + 1
+      
+       
+          end   #unless
+          
+          member = Member.new(:user => user, :roles => roles)
+          project.members << member
        end   # if else
        i = i + 1      
     end  #do
   
      
-      
+     flash[:notice] = "Added #{counter_new_projects} new projects and #{counter_new_users} new users !"
      
      
     
--- app/helpers/project_importer_helper.rb
+++ app/helpers/project_importer_helper.rb
@@ -9,7 +9,7 @@ module ProjectImporterHelper
       end
     end
     select_tag('repository_scm',
-               options_for_select(scm_options, repository.class.name.demodulize),
+               options_for_select(scm_options, repository.class.name.demodulize), :style => 'width: 158px;',
                :disabled => (repository && !repository.new_record?),
                :onchange => remote_function(
                   :url => {
