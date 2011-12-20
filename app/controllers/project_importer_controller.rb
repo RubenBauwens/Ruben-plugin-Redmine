c1283c3df67dc8e8d5e2b6c68ee36789b8fb7bbb
Ruben Bauwens <bauwens_ruben@hotmail.com>
2011-12-20 09:35:19 +0100

    Documentatie en code opgeruimd

--- app/controllers/project_importer_controller.rb
+++ app/controllers/project_importer_controller.rb
@@ -11,7 +11,8 @@ class ProjectImporterController < ApplicationController
   
 
   def index
-  # Project.delete_all
+  
+  #fill roles dropdownlists 
     @roles_imported_users = Role.find :all, :order => 'builtin, position'
     @roles_view = [["--- Not Included ---", '']]
     roles = Role.find :all, :order => 'builtin, position'
@@ -19,93 +20,91 @@ class ProjectImporterController < ApplicationController
     roles.each do |role|
       @roles_view << role
     end 
-         
+  #fill available groups       
     @groups = Group.active.find(:all)  
        
   end
 
   def match
-         
-    
-    
-    
+      
+   #get file as the parameter from first view
     file = params[:file]  
     if file 
-     session[:filename] = file.path
-    session[:group_roles] = params[:group]  
-    
-    
      
- @attrs = ["Username", "Lastname", "Firstname", "Email", "Groupname", "Password"]
- dismodules = []
+     session[:filename] = file.path
+     #fill array with elements which the user has to match with the correct header
+    @attrs = ["Username", "Lastname", "Firstname", "Email", "Groupname", "Password"]
  
- # fill array with disabled modules
-  unless params[:issue_tracking]
-    dismodules << :issue_tracking
-  end
+    dismodules = []
+ 
+ # fill array with the modules which the user has disabled
+    unless params[:issue_tracking]
+      dismodules << :issue_tracking
+    end
   
-  unless params[:time_tracking]
-    dismodules << :time_tracking
-  end
+    unless params[:time_tracking]
+      dismodules << :time_tracking
+    end
   
-  unless params[:news]
+    unless params[:news]
      dismodules << :news
-  end
-  unless params[:documents]
+    end
+    
+    unless params[:documents]
      dismodules << :documents
-  end
+    end
   
-  unless params[:files]
+    unless params[:files]
      dismodules << :files
-  end
+    end
   
-  unless params[:wiki]
+    unless params[:wiki]
      dismodules << :wiki
-  end
-  unless params[:repository]
+    end
+    
+    unless params[:repository]
      dismodules << :repository
-  end
-  unless params[:forums]
+    end
+   
+    unless params[:forums]
      dismodules << :forums
-  end
-  unless params[:calendar]
+    end
+    
+    unless params[:calendar]
      dismodules << :calendar
-  end
-  unless params[:gantt]
+    end
+  
+    unless params[:gantt]
      dismodules << :gantt
-  end
-  session[:disabledmodules] = dismodules
+    end
+  
+    session[:disabledmodules] = dismodules
+  
+  # fill array with trackers which the user has selected
+    trackers = []  
+    if params[:bug_tracker]
+      trackers << "Bug"
+    end
+    
+    if params[:feature_tracker]
+      trackers << "Feature"
+    end
+    if params[:support_tracker]
+      trackers << "Support"
+    end
+  
+ 
   
   
-  trackers = []  # fill array with selected trackers
- if params[:bug_tracker]
-   trackers << "Bug"
- end
- if params[:feature_tracker]
-   trackers << "Feature"
- end
-  if params[:support_tracker]
-    trackers << "Support"
-  end
-  
-  session[:selectedtrackers] = trackers
-  
-  
-  session[:repository] = params[:repository_scm]
-  session[:repository_base_url] = params[:repository_url]
-  
-  sample_count = 5
-  session[:users_role] = params[:role_user]
-  
-  i = 0
-  options = { :headers=>true, :return_headers => true }
-@headers = []
-@samples = []
+    options = { :headers=>true, :return_headers => true }
+    @headers = []
+    samples = []
       begin
-        @samples = FasterCSV.read(session[:filename], options).to_a
+        #Read the CSV file and fill the array with the headers
+        samples = FasterCSV.read(session[:filename], options).to_a
 
-        if @samples.size > 0
-        @headers = @samples.shift 
+        if samples.size > 0
+        @headers = samples.shift 
         end  
         
         if @headers.size < 6
@@ -113,144 +112,160 @@ class ProjectImporterController < ApplicationController
           The CSV-file must contain a column for: username, password, firstname, lastname, email and projectname!"
         end
       rescue FasterCSV::MalformedCSVError => e
-            flash[:error] = "Error in file! <br />
+            flash.now[:error] = "Error in file! <br />
             It has to be a CSV-file, and has to look like: <br />
             username,lastname,firstname,email,groupname,password  <br />
             801278,Mayer,John,john.mayer@example.com,projectname,smi748s
            "
             
       end # begin/rescue
+     #put parameters from first view in session attribute => is accessible in third view
+    
+    session[:group_roles] = params[:group]
+    session[:selectedtrackers] = trackers 
+    #session[:repository] = params[:repository_scm]
+    #session[:repository_base_url] = params[:repository_url]
+    session[:users_role] = params[:role_user] 
      
-     flash.discard(:error)
      
      
    else
-    
+      #if no file selected, go back to first view (user doesn't see this)
       redirect_to 'javascript:history.go(-1)'
       
    end
     
   
    end  #match
-   
-
-   
   
       def result
         
       
-        validation = false
-        repository = session[:repository]
-  repository_base_url = session[:repository_base_url]
-        replaced_users_count = 0
-        new_users = 0
-        trackers = []
-        handled_rows = 0
-        added_project_names = []
-        @failed_projects_array_strings = []
-        failed_existing_projects_count = 0
-      
-
+        #@project_repository = session[:repository]
         
-      @existing_projects = []
-      groups = Group.active.find(:all)  
+        
+        #get session attributes
+      #repository = session[:repository]
+      #repository_base_url = session[:repository_base_url]
       group_roles = session[:group_roles]
+      selected_trackers = session[:selectedtrackers]     
+      disabledmods =  session[:disabledmodules]
+      filename = session[:filename]
+      role_users = Role.find_by_name(session[:users_role])
+      
+      #variables
+      counter_new_users = 0
+      counter_new_projects = 0
+      validation = false
+      replaced_users_count = 0
+      new_users = 0
+      trackers = []
+       
+      added_project_names = []
+      @failed_projects_array_strings = []
+      failed_existing_projects_count = 0
+      #get map from matching and invert (keys become values, values become keys)
+      attrs_map = params[:fields_map].invert
+        
+      existing_projects = []
+      groups = Group.active.find(:all)  
+     
       x = 0
       group_hash = Hash.new
+      #make hash table with key = group, value = role
       group_roles.each do |role|
         if role != ""
           group_hash[groups[x]] = role
         end
         x = x + 1
-      end
-       selected_trackers = session[:selectedtrackers]
-
+      end       
+      
+      #trackers array filled with tracker objects, no strings
       selected_trackers.each do |tracker|
         trackers << Tracker.find_by_name(tracker)
       end
-      disabledmods =  session[:disabledmodules]
-     filename = session[:filename]
-      role_users = Role.find_by_name(session[:users_role])
+      
+     
       roles = []
       roles << role_users
-      attrs_map = params[:fields_map].invert
-     @project_repository = session[:repository]
+      
+    
       i = 0
+      #put headers in variables
       username_header = attrs_map['Username']
       lastname_header = attrs_map['Lastname']
       firstname_header = attrs_map['Firstname']
       password_header = attrs_map['Password']
       mail_header = attrs_map['Email']
       groupname_header = attrs_map['Groupname']
-      counter_new_users = 0
-      counter_new_projects = 0
-   begin
-     @headers = []
-      @samplestest = []   
-      begin
-     options = { :headers=>true, :return_headers => true }
-    @data = FasterCSV.read(filename, options).to_a
-    rescue Exception => e
-      flash.now[:error] = e.message
-    end
-if @data.size > 0
-  @headers = @data.shift 
-end  
-   
-   if !username_header or !lastname_header or !firstname_header or !password_header or !mail_header or !groupname_header
-     validation = false
-     
-   else
-     validation = true
-   end 
-       if validation
-       @index_username = @headers.index(username_header)
-       @index_password = @headers.index(password_header)
-       @index_lastname = @headers.index(lastname_header)
-       @index_firsname = @headers.index(firstname_header)
-       @index_mail = @headers.index(mail_header)
-       @index_groupname = @headers.index(groupname_header)
       
-     @data.each do |row|
-        project = Project.find_by_name(row[@index_groupname])
-       handled_rows = handled_rows + 1
-       if project
-         failed_existing_projects_count = failed_existing_projects_count + 1
-         if !added_project_names.include?(project.name)
-         @existing_projects << project
-         end
-       else
-         base_url = repository_base_url
-          project = Project.new
-          project.members = []
-          project.name = row[@index_groupname]
-          added_project_names << project.name
-          project.identifier = row[@index_groupname] 
-          project.is_public = false
-          disabledmods.each do |mod|
-            project.disable_module!(mod)
-          end
-          
-          project.trackers = trackers
-          @testgroep = Hash.new
-           @testgroep = []
+     begin
+     
+     headers = []
+     
+      begin
+      #Try to read CSV file again
+      options = { :headers=>true, :return_headers => true }
+      data = FasterCSV.read(filename, options).to_a
+      rescue Exception => e
+        flash.now[:error] = e.message
+      end
+
+      if data.size > 0
+        headers = data.shift 
+      end  
+      
+      #validation if matching was correct, if each necessary item has a header
+      if !username_header or !lastname_header or !firstname_header or !password_header or !mail_header or !groupname_header
+        validation = false     
+      else
+        validation = true
+      end 
+      
+       if validation
+       # get index of the headers and put in variable, used to read the correct column
+       @index_username = headers.index(username_header)
+       @index_password = headers.index(password_header)
+       @index_lastname = headers.index(lastname_header)
+       @index_firsname = headers.index(firstname_header)
+       @index_mail = headers.index(mail_header)
+       @index_groupname = headers.index(groupname_header)
+      
+        data.each do |row|
+          project = Project.find_by_name(row[@index_groupname])
+         
+          if project
+            #if project already exists, don't overwrite it, but put it as a warning
+            failed_existing_projects_count = failed_existing_projects_count + 1
+            if !added_project_names.include?(project.name)
+              existing_projects << project
+            end
+          else
+            #if project doesn't exist, make new with correct settings
+           
+            project = Project.new
+            project.members = []
+            project.name = row[@index_groupname]
+            added_project_names << project.name
+            project.identifier = row[@index_groupname] 
+            project.is_public = false
+            disabledmods.each do |mod|
+              project.disable_module!(mod)
+            end
+            
+            project.trackers = trackers
+          #add groups with their roles
           group_hash.each_pair do |group, role|
             m = Member.new
             m.principal = group
             m.project = project
             m.roles << Role.find_by_name(role)
-            project.members << m
-           
+            project.members << m           
           end # end do
           
-          #project.repository = @project_repository
-          #project.repository.save
-          project.save 
-          @naam = project.name
-       
-         @errors = project.errors.full_messages
+           project.save         
           counter_new_projects = counter_new_projects + 1      
-          repo = Repository.factory(repository)
+          #repo = Repository.factory(repository)
           #if repo and repository_base_url != ""
            # repo.root_url = repository_base_url
            #repo.project_id = project.identifier
@@ -266,45 +281,40 @@ end
         
          user = User.find_by_login(row[@index_username])
          if user
-         replaced_users_count = replaced_users_count + 1
-         User.delete(user)
+         #if user already exists, delete it and replace it by the user in the CSV file
+          replaced_users_count = replaced_users_count + 1
+          User.delete(user)
          else
            new_users = new_users + 1
-         end
-         
+         end         
         
-           user = User.new(:language => Setting.default_language, :mail_notification => Setting.default_notification_option)
-           user.login = row[@index_username]
-           user.password = row[@index_password]
-           user.lastname = row[@index_lastname]
-           user.firstname = row[@index_firsname]
-           user.password_confirmation = row[@index_password]
-           user.mail = row[@index_mail]
-           user.admin = 0
-           
-           
-          
-           user.save
-            member = Member.new(:user => user, :roles => roles)
-          project.members << member
+         user = User.new(:language => Setting.default_language, :mail_notification => Setting.default_notification_option)
+         user.login = row[@index_username]
+         user.password = row[@index_password]
+         user.lastname = row[@index_lastname]
+         user.firstname = row[@index_firsname]
+         user.password_confirmation = row[@index_password]
+         user.mail = row[@index_mail]
+         user.admin = 0  
+         user.save
+         #add newly created user to project
+         member = Member.new(:user => user, :roles => roles)
+         project.members << member
        
-      
-       
-       
-               
     end  #do
     
-    if @existing_projects.size > 0
+    if existing_projects.size > 0 #if some projects already exist, generate string with details
       flash.now[:warning] = "An error occurred, see details for more information!"
-     names = []   
-    @existing_projects.each do |pr|
+      names = []   
+      existing_projects.each do |pr|
         
         if !names.include?(pr.name)
-      @failed_projects_array_strings << "#{pr.name} already exists, added #{pr.members.size} users"
+          @failed_projects_array_strings << "#{pr.name} already exists, added #{pr.members.size} users"
         end #if
         names << pr.name 
-    end # do
+      end # do
     end #if 
+    
      flash.now[:notice] = "- Added <b><u>#{counter_new_projects}</u></b> new projects <br />- Added <b><u>#{new_users}</b></u> new users <br />- Overwritten <b><u>#{replaced_users_count}</b></u> users!"
    
      else
